956c7dca983b3eb2ff1b1b316f64b215ba48d62a
Tanishkaa23 <tanishkagupta203@gmail.com>
2026-07-05 01:07:00 +0530

    axios error fixed

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:5000/api',
+    baseURL: import.meta.env.VITE_API_URL,
     withCredentials: true   // REQUIRED — tells axios to send/receive cookies
 });
 
